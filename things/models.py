17b135e43e52fdfaa5a95455fd7a94d412a48266
k22040287 <ayan.mahmood@kcl.ac.uk>
2023-10-20 17:23:47 +0100

    Description now can be blank

--- things/models.py
+++ things/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.CharField(max_length=120)
+    description = models.CharField(max_length=120, blank=True)
     quantity = models.IntegerField(
         validators = [RegexValidator(
             regex = r'^([0-9]|[1-9][0-9]|100)$',
@@ -12,3 +12,4 @@ class Thing(models.Model):
             code = 'invalid_quantity'
         )]
     )
+    
