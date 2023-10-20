9dc57b2503c56ccc7d08651d1bef206bd88087d5
k22040287 <ayan.mahmood@kcl.ac.uk>
2023-10-20 17:01:00 +0100

    Exercise 2 and 3: add a basic Things model and refine Things model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,14 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 # Create your models here.
+class Thing(models.Model):
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120)
+    quantity = models.IntegerField(
+        validators = [RegexValidator(
+            regex = r'^([0-9]|[1-9][0-9]|100)$',
+            message = 'Quantity must be a number',
+            code = 'invalid_quantity'
+        )]
+    )
